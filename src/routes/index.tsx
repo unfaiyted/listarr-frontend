680414ed5f1aae918dc8358bbef09ab87f1bb011
Dane Miller <unfaiyted@gmail.com>
2024-12-16 14:22:11 -0600

    feature: implement ui handling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
+import { Popover } from "@kobalte/core/popover";
+// import { CrossIcon } from "some-icon-library";
+// import "./style.css";
 
 export default function Home() {
   return (
@@ -20,6 +23,25 @@ export default function Home() {
           About Page
         </A>{" "}
       </p>
+  <Popover>
+    <Popover.Trigger class="popover__trigger">Learn more</Popover.Trigger>
+    <Popover.Portal>
+      <Popover.Content class="popover__content">
+        <Popover.Arrow />
+        <div class="popover__header">
+          <Popover.Title class="popover__title">About Kobalte</Popover.Title>
+          <Popover.CloseButton class="popover__close-button">
+            {/* <CrossIcon /> */}
+                +
+          </Popover.CloseButton>
+        </div>
+        <Popover.Description class="popover__description">
+          A UI toolkit for building accessible web apps and design systems with SolidJS.
+        </Popover.Description>
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover>
     </main>
   );
 }
+
